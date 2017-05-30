0e00dae8405d586ef2976003dc3a8d31aa50633a
Jake Holtz <jahholtz@gmail.com>
2017-05-30 15:08:12 -0700

    Added routes/requests.js

---
+++ server/routes/requests.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+// router.route('/')
+//   .get((req, res) => {
+//     res.status(200)
+//   })
+//   .post((req, res) => {
+//     console.log('in the correct route');
+//     res.status(201)
+//   });
+
+module.exports = router;
