94f5f9f641e9f214b3d7e4ed1295a9e9cd5e051f
aybkao <aybkao@gmail.com>
2017-05-31 15:37:08 -0700

    delete double import

--- client/src/components/transactionsContainer.js
+++ client/src/components/transactionsContainer.js
@@ -4,7 +4,6 @@ import TransactionsList from './transactionsList.js';
 import AddTransactionsForm from './addTransactionsForm.js';
 import { Button, Header, Image, Modal, Checkbox, Form} from 'semantic-ui-react';
 import UploadReceipts from './uploadReceipts.js';
-import {Button} from 'semantic-ui-react';
 import store from '../store';
 import { connect } from 'react-redux';
 
