ec97cbe1fd5fd6e2f58342adf91de79ec15b8ed4
aybkao <aybkao@gmail.com>
2017-05-31 15:17:00 -0700

    fix merge conflicts

--- client/src/components/transactionsContainer.js
+++ client/src/components/transactionsContainer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TransactionsList from './transactionsList.js';
-// import {Button, Modal} from 'semantic-ui-react';
 import AddTransactionsForm from './addTransactionsForm.js';
 import { Button, Header, Image, Modal, Checkbox, Form} from 'semantic-ui-react';
+import UploadReceipts from './uploadReceipts.js';
+import {Button} from 'semantic-ui-react';
 import store from '../store';
 import { connect } from 'react-redux';
 import UploadReceipts from './uploadReceipts.js';
@@ -21,6 +22,7 @@ const TransactionsContainer = () => {
       <h2>Transactions</h2>
       <TransactionsList transactions={store.getState().transactions.transactions}/>
       <AddTransactionsForm />      
+      <Button fluid>Add Transaction</Button>
       <UploadReceipts />
     </div>
   );
