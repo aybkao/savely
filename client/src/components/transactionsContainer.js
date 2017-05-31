40ef7712631143cc4107b99bd0f94cf91cadfd34
aybkao <aybkao@gmail.com>
2017-05-31 15:11:36 -0700

    added UplaodReceipt component

--- client/src/components/transactionsContainer.js
+++ client/src/components/transactionsContainer.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TransactionsList from './transactionsList.js';
-import AddTransactionsForm from './addTransactionsForm.js'
-// import { Button, Header, Image, Modal, Checkbox, Form} from 'semantic-ui-react';
-import UploadReceipts from './uploadReceipts.js';
+// import {Button, Modal} from 'semantic-ui-react';
+import AddTransactionsForm from './addTransactionsForm.js';
+import { Button, Header, Image, Modal, Checkbox, Form} from 'semantic-ui-react';
 import store from '../store';
 import { connect } from 'react-redux';
+import UploadReceipts from './uploadReceipts.js';
 
 const getDate = () => {
   var today = new Date();
   return today.toDateString();
-}
+};
 
 //transactions={this.props.transactions}
 
