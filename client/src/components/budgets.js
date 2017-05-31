15a0c75ddcd83ee9083e5974c208d08bc2eabdd3
Ashwin Narasimhan <ashwinkn@gmail.com>
2017-05-30 20:03:00 -0700

    Added a Spending Categories Chart Container

--- client/src/components/budgets.js
+++ client/src/components/budgets.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SpendingCategoriesChartContainer from './spendingCategoriesChartContainer.js';
 
 const Budgets = () => {
   return (
     <div id="budgets_column" className="col-md-4">
       <h2>Spending Categories</h2>
+      <SpendingCategoriesChartContainer />
     </div>
   )
 };
