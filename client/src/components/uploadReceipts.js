3d9e5cf31fb790437b907220e0f62a198ab44031
aybkao <aybkao@gmail.com>
2017-06-01 00:30:29 -0700

    added get request to retrieve api key from server

--- client/src/components/uploadReceipts.js
+++ client/src/components/uploadReceipts.js
@@ -3,7 +3,7 @@ import Dropzone from 'react-dropzone';
 import {Button} from 'semantic-ui-react';
 import axios from 'axios';
 import Promise from 'bluebird';
-import keys from '../../../apiKeys.js';
+//import keys from '../../../apiKeys.js';
 
 class UploadReceipts extends React.Component {
   constructor(props) {
@@ -18,38 +18,41 @@ class UploadReceipts extends React.Component {
   }
 
   onImageDrop(acceptedFiles) {
-    // localStorage.setItem("imgData", imgData);
-    // var dataImage = localStorage.getItem('imgData');
-    // console.log(dataImage);
     acceptedFiles.forEach((file)=> {
       var fr = new FileReader();
       fr.onload = function(e) {
         var base64String = e.target.result.slice(23, e.target.result.length);
-        var apiKey = process.env.GOOGLE_CLOUD_VISION_API_KEY || keys.GOOGLE_CLOUD_VISION_API_KEY;
-        var googleUrl = 'https://vision.googleapis.com/v1/images:annotate?key=' + apiKey;
         axios({
-          method: 'post',
-          url: googleUrl,
-          data: {
-            "requests": [
-              {
-                "image": {
-                  "content": base64String
-                },
-                "features": [
+          method: 'get',
+          url: '/ocr'
+        })
+          .then(function(response) {
+            var googleUrl = 'https://vision.googleapis.com/v1/images:annotate?key=' + response.data; 
+            axios({
+              method: 'post',
+              url: googleUrl,
+              data: {
+                "requests": [
                   {
-                    "type": "TEXT_DETECTION"
+                    "image": {
+                      "content": base64String
+                    },
+                    "features": [
+                      {
+                        "type": "TEXT_DETECTION"
+                      }
+                    ]
                   }
                 ]
               }
-            ]
-          }
-        })
-          .then(function(response) {
-            var parsedText = response.data.responses[0].textAnnotations;
-            parsedText.forEach((line) => {
-              console.log(line.description);
-            });
+            })
+              .then(function(response) {
+                console.log('RES FROM API', response);
+                var parsedText = response.data.responses[0].textAnnotations;
+                parsedText.forEach((line) => {
+                  console.log(line.description);
+                });
+              });
           });
       };
       fr.readAsDataURL(file);
@@ -62,8 +65,6 @@ class UploadReceipts extends React.Component {
       <div>
         <div id="receipt-dropzone">
           <Dropzone
-            onDragEnter={this.dragEnter}
-            onDragLeave={this.dragLeave}
             onDrop={this.onImageDrop}
             name='file' id="dropped" ref="dropped">
             <div>Drag and Drop a Receipt Image</div>
