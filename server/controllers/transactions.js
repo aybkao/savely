d199a4306889089e6bf3a1c0e79ee017cfa1d85e
Jake Holtz <jahholtz@gmail.com>
2017-06-02 16:44:07 -0700

    Updated routes

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -1,20 +1,24 @@
 const models = require('../../db/models');
 
-// module.exports.create = (req, res) => {
-//   models.Transactions.forge().save({
-//     vendor: 'Test',
-//     amount: 1,
-//     date: 'May 30 2017',
-//     category: 'Test',
-//     description: 'Test'
-//   }, {method:"update"})
-//     .then(() => {
-//       res.send('Test successful!')
-//     });
-// };
-//
+module.exports.create = (req, res) => {
+  models.Transactions.forge({
+    vendor: 'Test',
+    amount: 1,
+    date: 'May 30 2017',
+    category: 'Test',
+    description: 'Test'
+  })
+    .save(null, {method: 'insert'})
+    .then(function (user) {
+      res.json({status: 'Success! WOO!'});
+    })
+    .catch(function (err) {
+      res.status(500).json({status: 'Error! BOO'});
+    });
+};
+
 // // module.exports.create = (req, res) => {
-// //   models.Transactions.forge({vendor: 'Test', amount: 1, date: 'May 30 2017', category: 'Test', description: 'Test' })
+// //   models.Transactions.forge()
 // //     .fetch()
 // //     .then(result => {
 // //       res.status(201).send('Test successful!');
@@ -27,16 +31,16 @@ const models = require('../../db/models');
 // //     });
 // // };
 
-module.exports.create = (req, res) => {
-  models.Transactions.fetchAll()
-    .then(transactions => {
-      res.status(200).send(transactions);
-    })
-    .catch(err => {
-      // This code indicates an outside service (the database) did not respond in time
-      res.status(503).send(err);
-    });
-};
+// module.exports.create = (req, res) => {
+//   models.Transactions.fetchAll()
+//     .then(transactions => {
+//       res.status(200).send(transactions);
+//     })
+//     .catch(err => {
+//       // This code indicates an outside service (the database) did not respond in time
+//       res.status(503).send(err);
+//     });
+// };
 
 
 module.exports.getAll = (req, res) => {
