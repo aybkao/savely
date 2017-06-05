608086b0f171b378cb64afe8e118720a44aab8e1
aybkao <aybkao@gmail.com>
2017-06-04 17:22:34 -0700

    added reducers for transactions for redux store

--- client/src/reducers/transactions.js
+++ client/src/reducers/transactions.js
@@ -1,12 +1,35 @@
-function transactions(state=[], action) {
+import { TRANSACTIONS_REQUEST, TRANSACTIONS_SUCCESS ,TRANSACTIONS_FAILURE } from '../actions/types';
+
+export default function transactions(state={isFetching: false, transactions:[], err:''}, action) {
+  
   switch(action.type) {
-    case 'ADD_TRANSACTION':
-      const i = action.index;
-      console.log('transaction added');
-      return state;
+    // case ADD_TRANSACTION:
+    //   const i = action.index;
+    //   console.log('transaction added');
+    //   return state;
+    case TRANSACTIONS_REQUEST:
+      return {
+      	...state,
+      	isFetching: true
+      }
+
+    case TRANSACTIONS_SUCCESS:
+      return {
+      	...state, 
+      	transactions: action.transactions,
+      	isFetching: false
+      }
+    
+    case TRANSACTIONS_FAILURE:
+      return {
+        ...state, 
+        err: action.err,
+        isFetching: false
+      }
+
     default:
       return state;
   }
 }
 
-export default transactions;
+//export default transactions;
