d85e2d70d6d466fa773d7b5daaad2b392c88d9ec
Ashwin Narasimhan <ashwinkn@gmail.com>
2017-06-06 19:25:40 -0700

    Imported requirement UI components from Semantic

--- client/src/components/onboarding.js
+++ client/src/components/onboarding.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Form, Button, Dropdown} from 'semantic-ui-react';
 
 class Onboarding extends React.Component {
   constructor(props) {
@@ -12,6 +13,9 @@ class Onboarding extends React.Component {
         <img id='logoSavely' src='/assets/logoGreen.png'></img>
       </div>
       <h1>Welcome to Savely</h1>
+      <Form>
+        <h2>Enter your income:</h2>
+      </Form>
     </div>
     )
   }
