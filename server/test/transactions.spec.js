6422e2ffa1906d969df8e412766187c9d2e89d01
Jake Holtz <jahholtz@gmail.com>
2017-06-07 19:17:55 -0700

    Added additional transaction requests. Up to 16

--- server/test/transactions.spec.js
+++ server/test/transactions.spec.js
@@ -5,7 +5,16 @@ const expect = require('chai').expect;
 const app = require('../app.js');
 
 describe('TRANSACTIONS PAGE', function() {
-  it('should have a valid transaction page ("/transaction")', function(done) {
+
+  var testPostObj = {
+    "vendor": "Test",
+    "amount": 1,
+    "description": "Test",
+    "date": "2017-05-30",
+    "id": 21
+  };
+
+  it('transaction page ("/transaction") should exist', function(done) {
     request(app)
       .get('/transaction')
       .expect(function(res) {
@@ -24,21 +33,35 @@ describe('TRANSACTIONS PAGE', function() {
   });
 
   it('transactions page should accept valid post requests', function(done) {
+    request(app)
+      .post('/transaction')
+      .send(testPostObj)
+      .expect(201)
+      .end(done);
+  });
 
-    var testPostObj = {
-      "vendor": "Test",
-      "amount": 1,
-      "description": "Test",
-      "date": "2017-05-30",
-      "id": 21
-    };
-
+  it('post requests should have an object as body', function(done) {
     request(app)
       .post('/transaction')
       .send(testPostObj)
       .expect(function(res) {
-        expect(res.status).to.be.within(200, 201);
+        expect(res.body).to.be.an('object');
       })
       .end(done);
   });
+
+  it('should post fields for id, vendor, amount, description, date', function(done) {
+    request(app)
+      .post('/transaction')
+      .send(testPostObj)
+      .expect(function(res) {
+        expect(!!res.body.id).to.exist;
+        expect(!!res.body.vendor).to.exist;
+        expect(!!res.body.amount).to.exist;
+        expect(!!res.body.description).to.exist;
+        expect(!!res.body.date).to.exist;
+      })
+      .end(done);
+  });
+
 });
