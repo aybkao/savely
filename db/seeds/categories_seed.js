7fd8f4b78ef7b10bb242acd61edfb25d31a60e93
aybkao <aybkao@gmail.com>
2017-06-10 10:59:46 -0700

    adding seed data for knex

--- db/seeds/categories_seed.js
+++ db/seeds/categories_seed.js
@@ -1,34 +1,37 @@
 const models = require('../models');
 
-exports.seed = function (knex, Promise) {
+const cats = [
+  {
+    category: 'Restaurants',
+    budget_limit: 900,
+    profile_id: 1
+  },
+  {
+    category: 'Groceries',
+    budget_limit: 300,
+    profile_id: 1
+  },
+  {
+    category: 'Entertainment',
+    budget_limit: 400,
+    profile_id: 1
+  },
+  {
+    category: 'Clothing',
+    budget_limit: 500,
+    profile_id: 1
+  },
+  {
+    category: 'Housing',
+    budget_limit: 2000,
+    profile_id: 1
+  },
+  {
+    category: 'Cosmetics',
+    budget_limit: 100,
+    profile_id: 1
+  }
+];
 
-  return models.Category.where({ profile_id: 1 }).fetch()
-    .then((categories) => {
-      if (category) {
-        throw category;
-      }
-      return models.Category.forge({
-        name: '',
-        profile_id: 1,
-      }).save();
-    })
-    .error(err => {
-      console.error('ERROR: failed to create category');
-      throw err;
-    })
-    .then((profile) => {
-      return models.Auth.forge({
-        type: 'local',
-        password: 'admin123',
-        profile_id: profile.get('id')
-      }).save();
-    })
-    .error(err => {
-      console.error('ERROR: failed to create auth');
-    })
-    .catch(() => {
-      console.log('WARNING: defualt user already exists.');
-    });
 
-};
 
